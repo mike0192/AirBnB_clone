a7c3bdd3315dc9a174f04ffa5a3e5aa143386951
Bruka25 <bkteshome862@gmail.com>
2024-01-11 23:35:40 +0300

    update the init with *args and **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,26 @@ class BaseModel:
         instance methods
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Magic function for initializing attributes id, created_at,
            updated_at
+
+           *args: Tuple containg all the arguments but it's
+                 not used here
+           **kwargs: is a dictionary with key/value attributes
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """ dunder method that returns the human readable
