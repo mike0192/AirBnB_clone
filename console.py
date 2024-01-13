a965b7ee5b806ccc065e8123d9da8a2161b27aae
michael <michaelhagos163@gmail.com>
2024-01-13 08:51:09 -0800

    a console that contains the entry points of the command interpreter

--- console.py
+++ console.py
@@ -10,6 +10,42 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file),
+        and prints the id
+
+        Usage: create <class_name>
+        """
+
+        try:
+            if not args:
+                raise SyntaxError()
+
+            split = args.split(" ")
+            instance = eval("{}()".format(split[0]))
+
+            for cmd_arg in split[1:]:
+                parameter = cmd_arg.split("=")
+                key = parameter[0]
+                value = parameter[1].replace("_", " ")
+
+                if hasattr(instance, key):
+                    try:
+                        setattr(instance, key, eval(value))
+                    except Exception:
+                        pass
+
+            instance.save()
+
+            print("{}".format(instance.id))
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+        except IndexError:
+            pass
+
     def do_quit(self, arg):
         """Quit the program"""
         "Exit"
