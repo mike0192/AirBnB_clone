81a86bfc2a394dbc2124219ff823c468fc0d2f0c
Bruka25 <bkteshome862@gmail.com>
2024-01-13 23:52:28 +0300

    city class for the models

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for City class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class with a empty string public attributes
+        state_id and name
+    """
+
+    state_id = ""
+    name = ""
