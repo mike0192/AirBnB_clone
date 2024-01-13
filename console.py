0dc004936f3a9330a1ff6970bd70e7dd389f3587
Bruka25 <bkteshome862@gmail.com>
2024-01-13 21:36:21 +0300

    update the console with the command destroy

--- console.py
+++ console.py
@@ -4,11 +4,18 @@
    interpretation
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
+from shlex import split
 
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
+
+    model_classes = {
+            "BaseModel"
+            }
 
     def do_quit(self, args):
         """Quit the program"""
@@ -38,10 +45,10 @@ class HBNBCommand(cmd.Cmd):
             if not args:
                 raise SyntaxError()
 
-            split = args.split(" ")
-            instance = eval("{}()".format(split[0]))
+            split_args = args.split(" ")
+            instance = eval("{}()".format(split_args[0]))
 
-            for cmd_arg in split[1:]:
+            for cmd_arg in split_args[1:]:
                 parameter = cmd_arg.split("=")
                 key = parameter[0]
                 value = parameter[1].replace("_", " ")
@@ -61,6 +68,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         except IndexError:
             pass
+
     def do_show(self, args):
         """
         Command that prints the string representation of an instance
@@ -68,32 +76,69 @@ class HBNBCommand(cmd.Cmd):
 
         Usage: show <class_name> <id>
         """
-    try:
-        if not args:
-            raise SyntaxError()
+        try:
+            if not args:
+                raise SyntaxError()
 
-        split_args = args.split(" ")
+            split_args = args.split(" ")
 
-        if split_args[0] not in self.model_classes:
-            raise NameError()
+            if split_args[0] not in self.model_classes:
+                raise NameError()
 
-        if len(split_args) < 2:
-            raise IndexError()
+            if len(split_args) < 2:
+                raise IndexError()
 
-        model_obj = storage.all()
-        key = split_args[0] + '.' + split_args[1]
+            model_obj = storage.all()
+            key = split_args[0] + '.' + split_args[1]
 
-        if key in model_obj:
-            print(model_obj[key])
-        else:
-            raise KeyError()
-    except SyntaxError:
-        print("** class name missing **")
-    except NameError:
-        print("** class doesn't exist **")
-    except IndexError:
-        print("** instance id missing **")
-    except KeyError:
-        print("** no instance found **")
+            if key in model_obj:
+                print(model_obj[key])
+            else:
+                raise KeyError()
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+        except IndexError:
+            print("** instance id missing **")
+        except KeyError:
+            print("** no instance found **")
+
+    def do_destroy(self, args):
+        """Command that deletes an instance of a class name and id
+           and saves the change to the JSON file
+
+           Usage: destroy <class name> <id>
+        """
+        try:
+            if not args:
+                raise SyntaxError()
+
+            split_args = args.split(" ")
+
+            if split_args[0] not in self.model_classes:
+                raise NameError()
+
+            if len(split_args) < 2:
+                raise IndexError()
+
+            model_obj = storage.all()
+            key = split_args[0] + '.' + split_args[1]
+
+            if key in model_obj:
+                del model_obj[key]
+                storage.save()
+            else:
+                raise KeyError()
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
+        except IndexError:
+            print("** instance id missing **")
+        except KeyError:
+            print("** no instance found **")
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
